ce3f0ef58699b78c1245191c626395ea890e6bd0
Igor D <logerr23@gmail.com>
2023-03-17 15:57:53 +0300

    add tests

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,9 +1,12 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
@@ -15,31 +18,43 @@ class AuthTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
+        $x("//*[@data-test-id = 'login']//input").sendKeys(registeredUser.getLogin());
+        $x("//*[@data-test-id = 'password']//input").sendKeys(registeredUser.getPassword());
+        $x("//*[@data-test-id = 'action-login']").click();
+        $x("//*[contains(text(), 'Личный кабинет')]").shouldBe(Condition.visible);
     }
 
     @Test
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+        $x("//*[@data-test-id = 'login']//input").sendKeys(notRegisteredUser.getLogin());
+        $x("//*[@data-test-id = 'password']//input").sendKeys(notRegisteredUser.getPassword());
+        $x("//*[@data-test-id = 'action-login']").click();
+        $x("//*[@data-test-id = 'error-notification']")
+                .shouldHave(Condition.text("Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+        $x("//*[@data-test-id = 'login']//input").sendKeys(blockedUser.getLogin());
+        $x("//*[@data-test-id = 'password']//input").sendKeys(blockedUser.getPassword());
+        $x("//*[@data-test-id = 'action-login']").click();
+        $x("//*[@data-test-id = 'error-notification']")
+                .shouldHave(Condition.text("Пользователь заблокирован"))
+                .shouldBe(Condition.visible);
+
     }
 
     @Test
@@ -47,9 +62,12 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
+        $x("//*[@data-test-id = 'login']//input").sendKeys(wrongLogin);
+        $x("//*[@data-test-id = 'password']//input").sendKeys(registeredUser.getPassword());
+        $x("//*[@data-test-id = 'action-login']").click();
+        $x("//*[@data-test-id = 'error-notification']")
+                .shouldHave(Condition.text("Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -57,8 +75,11 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
+        $x("//*[@data-test-id = 'login']//input").sendKeys(registeredUser.getLogin());
+        $x("//*[@data-test-id = 'password']//input").sendKeys(wrongPassword);
+        $x("//*[@data-test-id = 'action-login']").click();
+        $x("//*[@data-test-id = 'error-notification']")
+                .shouldHave(Condition.text("Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible);
     }
 }
